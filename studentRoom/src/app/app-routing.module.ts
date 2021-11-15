13b7f90be2b66b3ea6806c9f9873431a616d248a
nathanmalone99 <61420616+nathanmalone99@users.noreply.github.com>
2021-11-15 20:54:02 +0000

    Added navbar component with styling

--- studentRoom/src/app/app-routing.module.ts
+++ studentRoom/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ListingsComponent } from './listings/listings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'listings', component: ListingsComponent},
+  {path: 'login', component: LoginComponent}
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
