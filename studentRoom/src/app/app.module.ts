ace2d05cb5aae0a59e70cd990ace0fe0c9d4124e
nathanmalone99 <61420616+nathanmalone99@users.noreply.github.com>
2021-11-15 20:02:45 +0000

    Added mutiple components

--- studentRoom/src/app/app.module.ts
+++ studentRoom/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { ListingsComponent } from './listings/listings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    ListingsComponent
   ],
   imports: [
     BrowserModule,
