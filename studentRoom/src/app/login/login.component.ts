f38f3081fb06a7e7ce1522cdec1fd7e071d02457
Adam <adamsaul314@gmail.com>
2021-11-26 13:55:22 +0000

    log in with firebase

--- studentRoom/src/app/login/login.component.ts
+++ studentRoom/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent  {
 
-  log(x: any) {console.log(x); }
+  title = 'studentRoom';
+  isSignedIn=false
+  constructor(public firebase : FirebaseService){}
+  ngOnInit(){
+    if(localStorage.getItem('user')!==null)
+    this.isSignedIn= true
+    else
+    this.isSignedIn=false
+  }
+  async onSignup(email:string,password:string){
+    await this.firebase.signup(email,password)
+    if(this.firebase.isLoggedIn)
+    this.isSignedIn=true
+  }
+  async onSignin(email:string,password:string){
+    await this.firebase.signup(email,password)
+    if(this.firebase.isLoggedIn)
+    this.isSignedIn=true
+  }
+  handleLogout(){
+    this.isSignedIn=false
+    
+  }
 
 }
