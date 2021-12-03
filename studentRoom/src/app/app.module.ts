1844b2006c4e903b5f15e9990424e5038d213ab8
Adam <adamsaul314@gmail.com>
2021-12-03 12:19:52 +0000

    firebase

--- studentRoom/src/app/app.module.ts
+++ studentRoom/src/app/app.module.ts
@@ -10,6 +10,17 @@ import { LoginComponent } from './login/login.component';
 import { ListingsComponent } from './listings/listings.component';
 import { FirebaseService } from './services/firebase.service';
 import { LoggedinComponent } from './loggedin/loggedin.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -31,9 +42,19 @@ import { LoggedinComponent } from './loggedin/loggedin.component';
       messagingSenderId: "775340784871",
       appId: "1:775340784871:web:8b8ba6530ec4a269423642",
       measurementId: "G-CW6N4C57BN"
-    })
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [FirebaseService],
+  providers: [FirebaseService, ScreenTrackingService,UserTrackingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
