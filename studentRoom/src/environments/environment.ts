418eb7c5242bab5fa5ad17b6a2f0edfa14f15792
nathanmalone99 <61420616+nathanmalone99@users.noreply.github.com>
2022-01-26 18:43:27 +0000

    Fixed database

--- studentRoom/src/environments/environment.ts
+++ studentRoom/src/environments/environment.ts
@@ -9,15 +9,13 @@ export const environment = {
 
   firebaseConfig: {
 
-    projectId: 'studentroom-f4ec0',
-    appId: '1:775340784871:web:8b8ba6530ec4a269423642',
-    storageBucket: 'studentroom-f4ec0.appspot.com',
-    locationId: 'europe-west2',
-    apiKey: 'AIzaSyCmYAzjzJRKE8aMWpBAqpEnlDSeuxur9M8',
-    authDomain: 'studentroom-f4ec0.firebaseapp.com',
-    messagingSenderId: '775340784871',
-    measurementId: 'G-CW6N4C57BN',
-    databaseURL: "https://studentroom-f4ec0-default-rtdb.europe-west1.firebasedatabase.app"
+  apiKey: "AIzaSyDg0gNV4-EyXMPeWUrZKnC-UqwxPp-JRLw",
+  authDomain: "prj300-31d5b.firebaseapp.com",
+  databaseURL: "https://prj300-31d5b-default-rtdb.firebaseio.com",
+  projectId: "prj300-31d5b",
+  storageBucket: "prj300-31d5b.appspot.com",
+  messagingSenderId: "1036182122826",
+  appId: "1:1036182122826:web:ca23534506098b215a91b3"
   }
 };
 
