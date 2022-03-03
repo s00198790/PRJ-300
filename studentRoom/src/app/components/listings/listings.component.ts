aba283f3518d26871d8ae107dbfb9f043e1b758c
Adam Saul <S00197531@mail.itsligo.ie>
2022-03-03 11:40:33 +0000

    listing compenent

--- studentRoom/src/app/components/listings/listings.component.ts
+++ studentRoom/src/app/components/listings/listings.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { propertiesList } from 'src/app/helpers/propertiesList';
+import { UiService } from 'src/app/services/ui.service';
+import {
+  elementAnimations,
+  ANIMATION_ELEMENT,
+} from 'src/app/animations/elementAnimations';
 
 @Component({
-  selector: 'app-listings',
+  selector: 'rl-listings',
   templateUrl: './listings.component.html',
-  styleUrls: ['./listings.component.css']
+  styleUrls: ['./listings.component.scss'],
+  animations: [elementAnimations],
 })
 export class ListingsComponent implements OnInit {
+  @HostBinding('@elementAnimations') animate;
+  animationElement = ANIMATION_ELEMENT;
 
-  constructor() { }
+  propertiesList = propertiesList;
 
-  ngOnInit(): void {
+  constructor(public ui: UiService) {}
+
+  ngOnInit(): void {}
+
+  animationDone(event) {
+    (event.element as HTMLDivElement).childNodes.forEach((item) => {
+      const node = item as HTMLDivElement;
+      if (node.classList && node.classList.contains('active')) {
+        node.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
   }
 
+  onNavigate() {
+    this.ui.openState.next('open');
+  }
 }
