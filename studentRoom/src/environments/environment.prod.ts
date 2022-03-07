57c59d28235a10eda45ec38920bf71aec1862f8e
nathanmalone99 <61420616+nathanmalone99@users.noreply.github.com>
2022-03-07 20:30:32 +0000

    Updated database

--- studentRoom/src/environments/environment.prod.ts
+++ studentRoom/src/environments/environment.prod.ts
@@ -3,12 +3,12 @@ export const environment = {
   production: true,
   firebaseConfig: {
 
-  apiKey: "AIzaSyCMwhkiJNd5eRoHoBzS28kgG6iW5hfwCis",
-  authDomain: "studentroom-bd0ca.firebaseapp.com",
-  databaseURL: "https://studentroom-bd0ca-default-rtdb.firebaseio.com",
-  projectId: "studentroom-bd0ca",
-  storageBucket: "studentroom-bd0ca.appspot.com",
-  messagingSenderId: "411994494734",
-  appId: "1:411994494734:web:a2da6ec1923807e36ae95f"
+    apiKey: "AIzaSyA06CERGvM90oM9GBvjgzwUmHnHWo76LI8",
+    authDomain: "studentroom-2.firebaseapp.com",
+    databaseURL: "https://studentroom-2-default-rtdb.firebaseio.com",
+    projectId: "studentroom-2",
+    storageBucket: "studentroom-2.appspot.com",
+    messagingSenderId: "1093188960154",
+    appId: "1:1093188960154:web:91d3d5913c925d94be0423"
   }
 };
