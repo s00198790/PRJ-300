68738b14eb75933fe636c24a7f73d9aeca7b189b
Ronan Keaveney <ronankeaveney123@gmail.com>
2022-03-13 18:02:02 +0000

    property id displayed in URL

--- studentRoom/src/app/property-list-item/property-list-item.component.ts
+++ studentRoom/src/app/property-list-item/property-list-item.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { PropertyService } from '../services/property.service';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { stringify } from 'querystring';
 
 interface Listing {
   payload: any;
@@ -17,33 +18,33 @@ interface Listing {
 export class PropertyListItemComponent implements OnInit {
   @Input()
   listing: Listing;
-  private basePath = '/uploads';
 
   constructor(public router: Router, private propertyService: PropertyService, private db: AngularFireDatabase, private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
   }
 
-  openListing(listing: Listing) {
-    localStorage.setItem('currentPropertyListing', JSON.stringify(listing));
-    this.router.navigate(['property']);
-  }
+  // openListing(listing: Listing) {
+  //   localStorage.setItem('currentPropertyListing', JSON.stringify(listing));
+  //   this.router.navigate(['property']);
+  // }
 
   deleteListing(listing: Listing): void {
     this.deleteFileDatabase(listing.id)
       .then(() => {
-        this.deleteFileStorage(listing.payload);
+        // this.deleteFileStorage(listing.payload);
       })
       .catch(error => console.log(error));
-      console.log(listing.id);
   }
 
   private deleteFileDatabase(id : string) {
     return this.db.list('imageDetails').remove(id);
   }
-  private deleteFileStorage(payload : string): void {
-    const storageRef = this.storage.ref(this.basePath);
-    storageRef.child(payload).delete();
-  }
 
+  openListing(listing: Listing) {
+    // localStorage.setItem('currentPropertyListing', JSON.stringify(listing));
+    this.router.navigate(['property/'], {
+      queryParams: { 'id': listing.id }
+    });
+  }
 }
